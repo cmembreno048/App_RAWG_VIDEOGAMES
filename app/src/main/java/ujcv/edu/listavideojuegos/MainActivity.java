5e2e8765200cc384bd5c4810f706dc84fbc99693
membreno <cmembreno048@gmail.com>
2020-07-21 08:03:05 -0600

    arreglando el bug del volley me faltaba definir el requestqueue

--- app/src/main/java/ujcv/edu/listavideojuegos/MainActivity.java
+++ app/src/main/java/ujcv/edu/listavideojuegos/MainActivity.java
@@ -10,6 +10,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,36 +30,30 @@ public class MainActivity extends AppCompatActivity {
     }
     private void obtenerDatosVolley() {
 
+        queue = Volley.newRequestQueue(this);
 
         String url = "https://api.androidhive.info/contacts/";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
-
                 try {
-
                     JSONArray mJsonArray = response.getJSONArray("contacts");
 
                     for (int i = 0 ; i<mJsonArray.length() ; i++){
-
                         JSONObject mJsonObj = mJsonArray.getJSONObject(i);
                         String name = mJsonObj.getString("name");
                         Toast.makeText(MainActivity.this, "Pokemos: "+name, Toast.LENGTH_SHORT).show();
                     }
-
                 }
                 catch (JSONException e) {
-
                     e.printStackTrace();
-
                 }
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
             }
         });
 
