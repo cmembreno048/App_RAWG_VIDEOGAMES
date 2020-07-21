32784a6d2466d751061108663878eacab1145d60
membreno <cmembreno048@gmail.com>
2020-07-21 08:43:16 -0600

    agregamos icono, y enlazamos con la api de RAWG games

--- app/src/main/java/ujcv/edu/listavideojuegos/MainActivity.java
+++ app/src/main/java/ujcv/edu/listavideojuegos/MainActivity.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -16,6 +17,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,18 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
         queue = Volley.newRequestQueue(this);
 
-        String url = "https://api.androidhive.info/contacts/";
+        String url = "https://rawg-video-games-database.p.rapidapi.com/games";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    JSONArray mJsonArray = response.getJSONArray("contacts");
+                    JSONArray mJsonArray = response.getJSONArray("results");
 
                     for (int i = 0 ; i<mJsonArray.length() ; i++){
                         JSONObject mJsonObj = mJsonArray.getJSONObject(i);
                         String name = mJsonObj.getString("name");
-                        Toast.makeText(MainActivity.this, "Pokemos: "+name, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "juegos: "+name, Toast.LENGTH_SHORT).show();
                     }
                 }
                 catch (JSONException e) {
@@ -55,7 +59,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
             }
-        });
+        })
+        {
+
+          public Map getHeaders() throws AuthFailureError{
+              HashMap headers = new HashMap();
+              headers.put("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com");
+              headers.put("x-rapidapi-key", "67036e1220msh05a699a10c679c1p151158jsn4d3d4d2bb094");
+              return headers;
+
+        }
+
+        };
 
         queue.add(request);
     }
